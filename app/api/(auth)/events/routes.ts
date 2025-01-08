24f761ff8bf1f6cd67d5fe36312e4cb3f59088a7
Amir Abaris <thisisamirabaris@gmail.com>
2025-01-08 13:27:38 +0330

    added api routes for mock jsons

--- app/api/(auth)/events/routes.ts
+++
@@ -1,7 +0,0 @@
-import events from "@/lib/events.json";
-
-import { NextApiResponse } from "next";
-
-export default function handler(res: NextApiResponse) {
-  res.status(200).json(events);
-}
---
+++ lib/events.ts
@@ -0,0 +1,10 @@
+export interface Event {
+  id: string;
+  title: string;
+  descriptions: string;
+  location: string;
+  data: string;
+  category: string;
+  ticketsAvailable: number;
+  price: number;
+}
